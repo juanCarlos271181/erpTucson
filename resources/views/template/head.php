63fa04458e34d638e2c72b46c0edb94e017013dd
jc271181jc <juanCarlos271181s@gmail.com>
2022-08-06 20:39:02 -0300

    dando estilos

--- resources/views/template/head.php
+++ resources/views/template/head.php
@@ -3,7 +3,7 @@
        <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/styles.css">
         <link rel="icon" type="image/x-icon" href="img/logoBlue.png" style="height: ">
         <script type="text/javascript" src="js/qrcodejs/qrcode.min.js"></script>
