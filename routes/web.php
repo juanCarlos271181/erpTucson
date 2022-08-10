f2371c733304f9bda9ba8e9e4a757f2d92fd144a
rogermunozweco <roger.munoz.ar@gmail.com>
2022-08-10 19:53:39 -0300

    added redirect

--- routes/web.php
+++ routes/web.php
@@ -19,58 +19,57 @@ use Illuminate\Support\Facades\Route;
     }
 
     Route::get('/', function () {
-        if(!isLogged())    return view('index');
         return view('index');
     });
 
 
     Route::get('/login', function () {
-        //if(!isLogged())    return view('index');
+        //if(!isLogged())    return redirect('/');
         return view('login');
     });
 
     Route::get('/inventario', function () {
-        if(!isLogged())    return view('index');
+        if(!isLogged())    return redirect('/');
         return view('inventario');
     });
 
     Route::get('/marmoleria', function () {
-        if(!isLogged())    return view('index');
+        if(!isLogged())    return redirect('/');
         return view('marmoleria');
     });
 
     Route::get('/menu_principal', function () {
-        if(!isLogged())    return view('index');
+        if(!isLogged()) return redirect('/'); 
         return view('menu_principal');
     });
 
     Route::get('/menu', function () {
-        if(!isLogged())    return view('index');
+        if(!isLogged())    return redirect('/');
         return view('menu');
     });
 
     Route::get('/pegamentos', function () {
-        if(!isLogged())    return view('index');
+        if(!isLogged())    return redirect('/');
         return view('pegamentos');
     });
 
     Route::get('/perfil_usuario', function () {
-        if(!isLogged())    return view('index');
+        if(!isLogged())    return redirect('/');
         return view('perfil_usuario');
     });
 
     Route::get('/prueba_validacion', function () {
-        if(!isLogged())    return view('index');
+        if(!isLogged())    return redirect('/');
         return view('prueba_validacion');
     });
 
     Route::get('/registro_usuario', function () {
-        if(!isLogged())    return view('index');
+        if(!isLogged())    return redirect('/');
         return view('registro_usuario');
     });
 
     Route::get('/griferia', function () {
-        if(!isLogged())    return view('index');
+        if(!isLogged())    return redirect('/');
         return view('griferia');
     });
 
