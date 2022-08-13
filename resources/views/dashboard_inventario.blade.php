0e34ea35712def99c83a52bbf632a9af809ce4e1
rogermunozweco <roger.munoz.ar@gmail.com>
2022-08-13 17:46:22 -0300

    fix dashboard inventario

--- resources/views/dashboard_inventario.blade.php
+++ resources/views/dashboard_inventario.blade.php
@@ -1,6 +1,6 @@
         <div class="contenedorMenu">
 
-            <div class="col-12" id="card_usuario_listado">
+            <div class="col-12" id="card_inventario_listado">
                 <div class="card" style="width:10rem; padding:1px">
                     <a href="./inventario_listado" class="button">
                         <img src="./img/inventario.webp" class="card-img-top " alt="imagen de tarjeta">
@@ -11,4 +11,4 @@
                 </div>
             </div>
         
-        </div>   
+        </div>  
