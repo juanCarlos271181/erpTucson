9296f6fff981d61bda77f04d8127e3bf00c56732
rogermunozweco <roger.munoz.ar@gmail.com>
2022-08-12 22:26:02 -0300

    ajustes menu

--- resources/views/template/header.php
+++ resources/views/template/header.php
@@ -12,7 +12,6 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 <?php if(isset($_SESSION["user"])){ ?>
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="./">Home</a></li>
                         <li class="nav-item"> <img src="./img/usuario2.jpg" width="30px" ></li>
                         <li class="nav-link"> 
                             <select id="action_user">
